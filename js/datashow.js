5502e5c365e38663c95cca33d563513e493bb05a
ShearWall-confine <148940950+ShearWall-confine@users.noreply.github.com>
2023-10-29 22:25:50 +0800

    11

--- js/datashow.js
+++ js/datashow.js
@@ -10,19 +10,18 @@ $(document).ready(function() {
             var lines = data.split('\n');
             var firstRow = lines[0].split(',');
             var table2 = document.getElementById('table2');
-            var table3 = document.getElementById('table3');
             for (var i = 0; i < firstRow.length; i++) 
             {
                 var row2 = table2.insertRow();
                 var cell = row2.insertCell();
                 cell.innerHTML = firstRow[i];
             }
-            for (var j = 0; j < lines.length; j++) 
+            for (var j = 1; j < lines.length; j++) 
             {
                 var cells = lines[j].split(',');
                 if (cells[0] === cellData) 
                 {
-                    for (var k = 0; k < cells.length; k++) 
+                    for (var k = 1; k < cells.length; k++) 
                     {
                         var cell2 = table2.rows[k].insertCell();
                         cell2.innerHTML = cells[k];
